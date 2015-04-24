294d5aa08fa21cd61d2a074bc116a1d140d64ea5
bohnje <bohnje3@gmail.com>
2015-04-23 20:53:18 -0500

    test

---
+++ StrawberryJM/Assets/DoorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	public bool isLocked;
+	private Lock attachedLock;
+
+	// Use this for initialization
+	void Start () {
+		attachedLock = gameObject.GetComponent<Lock>();
+
+		if(attachedLock != null)
+		{
+			isLocked = attachedLock.IsLocked();
+		}
+		else
+		{
+			isLocked = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//check if no longer locked
+		if(isLocked)
+		{
+			isLocked = attachedLock.IsLocked();
+
+		}
+	}
+}
---
+++ StrawberryJM/Assets/Lock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lock : MonoBehaviour {
+
+	private bool locked;
+
+	// Use this for initialization
+	void Start () {
+		locked = true;
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Key")
+		{
+			Object.Destroy(col.gameObject);
+			locked = false;
+		}
+	}
+
+	public bool IsLocked()
+	{
+		return locked;
+	}
+}
